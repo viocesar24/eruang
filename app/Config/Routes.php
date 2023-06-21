57ff10628aad11d586b3defe6a1e7fe302339e2d
VIO CESAR MELANO YONANTA <viocesar24@gmail.com>
2023-06-21 15:05:33 +0700

    Update Routes.php
    Bug rute diperbaiki

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,41 +37,45 @@ use App\Controllers\Peminjaman;
 use App\Controllers\User;
 
 $routes->match(['get', 'post'], 'peminjaman', [Peminjaman::class, 'index']);
-$routes->match(['get', 'post'], 'peminjaman/create/(:segment)', [Peminjaman::class, 'create']);
 $routes->match(['get', 'post'], 'peminjaman/create', [Peminjaman::class, 'create']);
-$routes->match(['get', 'post'], 'peminjaman/edit/(:segment)', [Peminjaman::class, 'edit']);
 $routes->match(['get', 'post'], 'peminjaman/edit', [Peminjaman::class, 'edit']);
-$routes->match(['get', 'post'], 'peminjaman/end/(:segment)', [Peminjaman::class, 'end']);
 $routes->match(['get', 'post'], 'peminjaman/end', [Peminjaman::class, 'end']);
-$routes->match(['get', 'post'], 'peminjaman/hapus/(:segment)', [Peminjaman::class, 'hapus']);
 $routes->match(['get', 'post'], 'peminjaman/hapus', [Peminjaman::class, 'hapus']);
 $routes->match(['get', 'post'], 'view', [Peminjaman::class, 'viewPeminjamanUser']);
 
-$routes->match(['get', 'post'], 'login/(:segment)', [User::class, 'login']);
 $routes->match(['get', 'post'], 'login', [User::class, 'login']);
-$routes->match(['get', 'post'], 'signup/(:segment)', [User::class, 'signup']);
 $routes->match(['get', 'post'], 'signup', [User::class, 'signup']);
-$routes->match(['get', 'post'], 'logout/(:segment)', [User::class, 'logout']);
 $routes->match(['get', 'post'], 'logout', [User::class, 'logout']);
-$routes->match(['get', 'post'], 'profile/(:segment)', [User::class, 'profile']);
 $routes->match(['get', 'post'], 'profile', [User::class, 'profile']);
 $routes->match(['get', 'post'], 'user', [User::class, 'index']);
-$routes->match(['get', 'post'], 'changePassword/(:segment)', [User::class, 'changePassword']);
 $routes->match(['get', 'post'], 'changePassword', [User::class, 'changePassword']);
-$routes->match(['get', 'post'], 'edit/(:segment)', [User::class, 'edit']);
 $routes->match(['get', 'post'], 'edit', [User::class, 'edit']);
-$routes->match(['get', 'post'], 'hapus/(:segment)', [User::class, 'hapus']);
 $routes->match(['get', 'post'], 'hapus', [User::class, 'hapus']);
-$routes->match(['get', 'post'], 'signupadmin/(:segment)', [User::class, 'signupadmin']);
 $routes->match(['get', 'post'], 'signupadmin', [User::class, 'signupadmin']);
 
-$routes->match(['get', 'post'], 'ruangan/(:segment)', [Ruangan::class, 'view']);
 $routes->match(['get', 'post'], 'ruangan', [Ruangan::class, 'index']);
 $routes->match(['get', 'post'], 'ruangan/create', [Ruangan::class, 'create']);
 
-$routes->match(['get', 'post'], 'pegawai/(:segment)', [Pegawai::class, 'view']);
 $routes->match(['get', 'post'], 'pegawai', [Pegawai::class, 'index']);
 $routes->match(['get', 'post'], 'pegawai/create', [Pegawai::class, 'create']);
+
+$routes->match(['get', 'post'], 'peminjaman/create/(:segment)', [Peminjaman::class, 'create']);
+$routes->match(['get', 'post'], 'peminjaman/edit/(:segment)', [Peminjaman::class, 'edit']);
+$routes->match(['get', 'post'], 'peminjaman/end/(:segment)', [Peminjaman::class, 'end']);
+$routes->match(['get', 'post'], 'peminjaman/hapus/(:segment)', [Peminjaman::class, 'hapus']);
+
+$routes->match(['get', 'post'], 'login/(:segment)', [User::class, 'login']);
+$routes->match(['get', 'post'], 'signup/(:segment)', [User::class, 'signup']);
+$routes->match(['get', 'post'], 'logout/(:segment)', [User::class, 'logout']);
+$routes->match(['get', 'post'], 'profile/(:segment)', [User::class, 'profile']);
+$routes->match(['get', 'post'], 'changePassword/(:segment)', [User::class, 'changePassword']);
+$routes->match(['get', 'post'], 'edit/(:segment)', [User::class, 'edit']);
+$routes->match(['get', 'post'], 'hapus/(:segment)', [User::class, 'hapus']);
+$routes->match(['get', 'post'], 'signupadmin/(:segment)', [User::class, 'signupadmin']);
+
+$routes->match(['get', 'post'], 'ruangan/(:segment)', [Ruangan::class, 'view']);
+$routes->match(['get', 'post'], 'pegawai/(:segment)', [Pegawai::class, 'view']);
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
