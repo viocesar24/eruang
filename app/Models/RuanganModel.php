784546edd3047e63cebeb837828585a2764ac878
VIO CESAR MELANO YONANTA <viocesar24@gmail.com>
2023-10-16 15:05:34 +0700

    membuka kembali ruangan auditorial dan konsultasi

--- app/Models/RuanganModel.php
+++ app/Models/RuanganModel.php
@@ -10,23 +10,23 @@ class RuanganModel extends Model
 
     protected $allowedFields = ['nama'];
 
-    // public function getRuangan($id = false)
-    // {
-    //     if ($id === false) {
-    //         return $this->findAll();
-    //     }
-
-    //     return $this->where(['id' => $id])->first();
-    // }
-
     public function getRuangan($id = false)
     {
         if ($id === false) {
-            // Menggunakan whereNotIn untuk mengecualikan ruangan "auditorial"
-            return $this->whereNotIn('nama', ['auditorial', 'konsultasi'])->findAll();
+            return $this->findAll();
         }
 
         return $this->where(['id' => $id])->first();
     }
+
+    // public function getRuangan($id = false)
+    // {
+    //     if ($id === false) {
+    //         // Menggunakan whereNotIn untuk mengecualikan ruangan "auditorial"
+    //         return $this->whereNotIn('nama', ['auditorial', 'konsultasi'])->findAll();
+    //     }
+
+    //     return $this->where(['id' => $id])->first();
+    // }
 
 }
