3091288e4fcac958fffb13cd8361643b63a4b077
VIO CESAR MELANO YONANTA <viocesar24@gmail.com>
2024-03-14 13:55:35 +0700

    penambahan fungsi delete pada halaman pegawai untuk admin

--- app/Controllers/Pegawai.php
+++ app/Controllers/Pegawai.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
 use App\Models\PegawaiModel;
+use App\Models\UserModel;
+use App\Models\PeminjamanModel;
 
 class Pegawai extends BaseController
 {
@@ -91,5 +93,57 @@ class Pegawai extends BaseController
         return view('templates/header', ['title' => 'Create a pegawai item'])
             . view('pegawai/success')
             . view('templates/footer');
+    }
+
+    public function delete()
+    {
+        helper('form');
+
+        // Cek apakah session pegawai_id ada dan apakah memiliki nilai 58 atau 35
+        if (!session()->has('pegawai_id') || (session()->get('pegawai_id') != 58 && session()->get('pegawai_id') != 35)) {
+            session()->setFlashdata('pegawaiError', 'Anda tidak diperkenankan menghapus data pegawai.');
+            return redirect()->to('/peminjaman');
+        }
+
+        $modelPegawai = model(PegawaiModel::class);
+        $modelPeminjaman = model(PeminjamanModel::class);
+        $modelUser = model(UserModel::class);
+
+        // Cek apakah metode request adalah POST dan validasi input
+        if ($this->request->getMethod() === 'post' && $this->validate(['id' => 'required|is_natural_no_zero'])) {
+            $id = $this->request->getPost('id');
+
+            // Cek apakah ID pegawai ada di database sebelum menghapus
+            if ($modelPegawai->find($id) !== null) {
+                // Cek apakah ID pegawai ada di tabel Peminjaman sebelum menghapus
+                if ($modelPeminjaman->where('id_pegawai', $id)->first()) {
+                    // Hapus data terkait dari tabel peminjaman
+                    $modelPeminjaman->where('id_pegawai', $id)->delete();
+                }
+
+                // Cek apakah ID pegawai ada di tabel User sebelum menghapus
+                if ($modelUser->where('pegawai_id', $id)->first()) {
+                    // Hapus data terkait dari tabel user
+                    $modelUser->where('pegawai_id', $id)->delete();
+                }
+
+                // Hapus data pegawai
+                $modelPegawai->delete($id);
+
+                // Cek apakah ada error saat penghapusan
+                if ($modelPegawai->errors()) {
+                    session()->setFlashdata('pegawaiError', 'Terjadi kesalahan saat penghapusan');
+                    return redirect()->to('/pegawai');
+                }
+
+                session()->setFlashdata('pegawaiSuccess', 'Data pegawai dan data terkait berhasil dihapus.');
+            } else {
+                session()->setFlashdata('pegawaiError', 'ID pegawai tidak ditemukan.');
+            }
+            return redirect()->to('/pegawai');
+        } else {
+            // Jika bukan POST atau validasi gagal, arahkan kembali ke halaman pegawai
+            return redirect()->to('/pegawai');
+        }
     }
 }
--- app/Views/pegawai/index.php
+++ app/Views/pegawai/index.php
@@ -1,3 +1,19 @@
+<?php if (session()->has('pegawaiSuccess')): ?>
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <div>
+            <?= session()->getFlashdata('pegawaiSuccess') ?>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php elseif (session()->has('pegawaiError')): ?>
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <div>
+            <?= session()->getFlashdata('pegawaiError') ?>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php endif; ?>
+
 <h2>
     <?= esc($title) ?>
 </h2>
@@ -12,6 +28,38 @@
 
         <div class="main">
             <?= esc($pegawai_item['nama']) ?>
+        </div>
+
+        <a type="button" class="btn btn-primary" href="/pegawai/<?= esc($pegawai_item['id']) ?>">Lihat</a>
+
+        <!-- Button trigger modal -->
+        <a type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#hapuspegawaimodal<?= esc($pegawai_item['id']) ?>">
+            Hapus
+        </a>
+
+        <!-- Modal -->
+        <div class="modal fade" id="hapuspegawaimodal<?= esc($pegawai_item['id']) ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="hapuspegawaimodal<?= esc($pegawai_item['id']) ?>Label" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="hapuspegawaimodal<?= esc($pegawai_item['id']) ?>Label">Hapus Pegawai</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Apakah Anda yakin ingin menghapus data Pegawai dengan Nama:<br>
+                        <b><?= esc($pegawai_item['nama']) ?></b>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                        <!-- Form untuk menghapus pegawai -->
+                        <form action="/pegawai/delete" method="post">
+                            <?= csrf_field() ?>
+                            <input type="hidden" name="id" value="<?= esc($pegawai_item['id']) ?>" />
+                            <button type="submit" class="btn btn-primary">Iya</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
 
     <?php endforeach ?>
